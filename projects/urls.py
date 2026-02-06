9f599e4566d216e4b794aa3976040748e2f10e1c
EHAB EMAD <ehabemadaboshady@gmail.com>
2026-02-06 08:59:26 +0200

    Comment out silk URL path
    Comment out the silk URL path in urlpatterns

--- projects/urls.py
+++ projects/urls.py
@@ -27,7 +27,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('api.urls')),
-    path('silk/' , include('silk.urls',namespace='silk')),
+    # path('silk/' , include('silk.urls',namespace='silk')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
